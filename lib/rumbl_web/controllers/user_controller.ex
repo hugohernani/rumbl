a01b98f87101fa11abe2d9e4f9ae24bbfc951e03
Hugo Hernani <hhernanni@gmail.com>
2021-02-23 03:32:42 -0300

    Add new and create routes for User model

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,13 +1,35 @@
 defmodule RumblWeb.UserController do
   use RumblWeb, :controller
 
+  alias Rumbl.User
+  alias Rumbl.Repo
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: Routes.user_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
   def index(conn, _params) do
-    users = Rumbl.Repo.all(Rumbl.User)
+    users = Repo.all(User)
     render(conn, "index.html", users: users)
   end
 
   def show(conn, %{"id" => id}) do
-    user = Rumbl.Repo.get(Rumbl.User, id)
+    user = Repo.get(User, id)
     render(conn, "show.html", user: user)
   end
 end
--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -16,9 +16,8 @@ defmodule RumblWeb.Router do
   scope "/", RumblWeb do
     pipe_through :browser
 
-    get "/users", UserController, :index
-    get "/users/:id", UserController, :show
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :new, :show, :create]
   end
 
   # Other scopes may use custom stacks.
