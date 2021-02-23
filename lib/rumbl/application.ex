db5c1867261c86edaddf2f46b64da6a7b3ecfe71
Hugo Hernani <hhernanni@gmail.com>
2021-02-22 22:19:31 -0300

    Create db table users

--- lib/rumbl/application.ex
+++ lib/rumbl/application.ex
@@ -8,7 +8,7 @@ defmodule Rumbl.Application do
   def start(_type, _args) do
     children = [
       # Start the Ecto repository
-      # Rumbl.Repo,
+      Rumbl.Repo,
       # Start the Telemetry supervisor
       RumblWeb.Telemetry,
       # Start the PubSub system
--- lib/rumbl/user.ex
+++ lib/rumbl/user.ex
@@ -1,3 +1,14 @@
 defmodule Rumbl.User do
-  defstruct [:id, :name, :username, :password]
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Rumbl.User
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
 end
