1ff06d0978af45de2174c7cb7fa48874b1e07f53
Hugo Hernani <hhernanni@gmail.com>
2021-02-24 03:09:51 -0300

    ADd registration fn into UserController

--- lib/rumbl/user.ex
+++ lib/rumbl/user.ex
@@ -1,7 +1,6 @@
 defmodule Rumbl.User do
   use Ecto.Schema
   import Ecto.Changeset
-  alias Rumbl.User
 
   schema "users" do
     field :name, :string
@@ -12,9 +11,27 @@ defmodule Rumbl.User do
     timestamps()
   end
 
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, [:password], [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
   def changeset(model, params \\ %{}) do
     model
     |> cast(params, [:name, :username], [])
     |> validate_length(:username, min: 1, max: 20)
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
