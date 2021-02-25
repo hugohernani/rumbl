f2f335697e1658c5929a2756064ff542000e3070
Hugo Hernani <hhernanni@gmail.com>
2021-02-25 02:02:58 -0300

    Refactor video controller to filter videos by logged user

--- lib/rumbl_web/controllers/video_controller.ex
+++ lib/rumbl_web/controllers/video_controller.ex
@@ -1,62 +1,101 @@
 defmodule RumblWeb.VideoController do
   use RumblWeb, :controller
 
+  alias Rumbl.Repo
   alias Rumbl.Videos
   alias Rumbl.Videos.Video
 
-  def index(conn, _params) do
-    videos = Videos.list_videos()
+  def action(conn, _) do
+    apply(__MODULE__, action_name(conn), [conn, conn.params, conn.assigns.current_user])
+  end
+
+  def index(conn, _params, user) do
+    videos =
+      user
+      |> user_videos
+      |> Repo.all()
+
     render(conn, "index.html", videos: videos)
   end
 
-  def new(conn, _params) do
-    changeset = Videos.change_video(%Video{})
+  def new(conn, _params, user) do
+    changeset =
+      user
+      |> Ecto.build_assoc(:videos)
+      |> Video.changeset(%{})
+
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"video" => video_params}) do
-    case Videos.create_video(video_params) do
-      {:ok, video} ->
+  def create(conn, %{"video" => video_params}, user) do
+    changeset =
+      user
+      |> Ecto.build_assoc(:videos)
+      |> Video.changeset(video_params)
+
+    case Repo.insert(changeset) do
+      {:ok, _video} ->
         conn
         |> put_flash(:info, "Video created successfully.")
-        |> redirect(to: Routes.video_path(conn, :show, video))
+        |> redirect(to: Routes.video_path(conn, :index))
 
-      {:error, %Ecto.Changeset{} = changeset} ->
+      {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    video = Videos.get_video!(id)
+  def show(conn, %{"id" => id}, user) do
+    video =
+      user
+      |> user_videos
+      |> Repo.get!(id)
+
     render(conn, "show.html", video: video)
   end
 
-  def edit(conn, %{"id" => id}) do
-    video = Videos.get_video!(id)
+  def edit(conn, %{"id" => id}, user) do
+    video =
+      user
+      |> user_videos
+      |> Repo.get!(id)
+
     changeset = Videos.change_video(video)
     render(conn, "edit.html", video: video, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "video" => video_params}) do
-    video = Videos.get_video!(id)
+  def update(conn, %{"id" => id, "video" => video_params}, user) do
+    video =
+      user
+      |> user_videos
+      |> Repo.get!(id)
 
-    case Videos.update_video(video, video_params) do
+    changeset = Video.changeset(video, video_params)
+
+    case Repo.update(changeset) do
       {:ok, video} ->
         conn
         |> put_flash(:info, "Video updated successfully.")
         |> redirect(to: Routes.video_path(conn, :show, video))
 
-      {:error, %Ecto.Changeset{} = changeset} ->
+      {:error, changeset} ->
         render(conn, "edit.html", video: video, changeset: changeset)
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    video = Videos.get_video!(id)
-    {:ok, _video} = Videos.delete_video(video)
+  def delete(conn, %{"id" => id}, user) do
+    video =
+      user
+      |> user_videos
+      |> Repo.get!(id)
+
+    Repo.delete!(video)
 
     conn
     |> put_flash(:info, "Video deleted successfully.")
     |> redirect(to: Routes.video_path(conn, :index))
+  end
+
+  defp user_videos(user) do
+    Ecto.assoc(user, :videos)
   end
 end
