9fc7a638e5a08c22387690c3938a6a536df8eca0
Hugo Hernani <hhernanni@gmail.com>
2021-02-28 02:22:17 -0300

    Add slug into Video model

--- lib/rumbl/videos/video.ex
+++ lib/rumbl/videos/video.ex
@@ -4,6 +4,7 @@ defmodule Rumbl.Videos.Video do
 
   schema "videos" do
     field :url, :string
+    field :slug, :string
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User
@@ -17,6 +18,27 @@ defmodule Rumbl.Videos.Video do
     video
     |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+end
+
+defimpl Phoenix.Param, for: Rumbl.Videos.Video do
+  def to_param(%{slug: slug, id: id}) do
+    "#{id}-#{slug}"
   end
 end
