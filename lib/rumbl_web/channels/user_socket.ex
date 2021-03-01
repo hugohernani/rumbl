c01bdc05bc0dc3b18aaeb6c033d04b12f6e5015f
Hugo Hernani <hhernanni@gmail.com>
2021-03-01 19:15:46 -0300

    Add token (created by Phoenix.Token) into conn.assigns and socket.assigns

--- lib/rumbl_web/channels/user_socket.ex
+++ lib/rumbl_web/channels/user_socket.ex
@@ -3,11 +3,21 @@ defmodule RumblWeb.UserSocket do
 
   channel "videos:*", RumblWeb.VideoChannel
 
+  @max_age 2 * 7 * 24 * 60 * 60
+
   @impl true
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket, _connect_info) do
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: @max_age) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        :error
+    end
   end
 
   @impl true
-  def id(_socket), do: nil
+  def connect(_params, _socket), do: :error
+
+  @impl true
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
 end
--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -14,12 +14,11 @@ defmodule Rumbl.Auth do
     user_id = get_session(conn, :user_id)
 
     cond do
-      conn.assigns[:current_user] ->
-        conn
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
 
       user = user_id && repo.get(User, user_id) ->
-        assign(conn, :current_user, user)
-
+        put_current_user(conn, user)
       true ->
         assign(conn, :current_user, nil)
     end
@@ -55,12 +54,20 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
 
   def logout(conn) do
     configure_session(conn, drop: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 end
