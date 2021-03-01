ebd34590a25ef7e82275452b0475c6e464978406
Hugo Hernani <hhernanni@gmail.com>
2021-03-01 18:53:03 -0300

    Add video channel communication without user token

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -1,7 +1,18 @@
 defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
 
-  def join("videos:" <> video_id, _params, socket) do
-    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+  def join("videos:" <> _video_id, _params, socket) do
+    {:ok, socket}
+  end
+
+  def handle_in("new_annotation", params, socket) do
+    IO.inspect(params)
+    broadcast! socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    }
+
+    {:reply, :ok, socket}
   end
 end
