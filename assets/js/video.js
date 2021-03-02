8c13508fb937976739dd569d76016598a58177d9
Hugo Hernani <hhernanni@gmail.com>
2021-03-02 00:11:35 -0300

    Load videos by the last seen video id

--- assets/js/video.js
+++ assets/js/video.js
@@ -34,11 +34,14 @@ let Video = {
     })
 
     vidChannel.on("new_annotation", (resp) => {
+      vidChannel.params.last_seen_id = resp.id
       this.renderAnnotation(msgContainer, resp)
     })
 
     vidChannel.join()
       .receive("ok", (resp) => {
+        let ids = resp.annotations.map(ann => ann.id)
+        if(ids.length > 0){ vidChannel.params.last_seen_id = Math.max(...ids) }
         this.scheduleMessages(msgContainer, resp.annotations)
       })
       .receive("error", reason => console.log("join failed", reason))
--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -4,12 +4,14 @@ defmodule RumblWeb.VideoChannel do
   alias Rumbl.Repo
   import Ecto.Query
 
-  def join("videos:" <> video_id, _params, socket) do
+  def join("videos:" <> video_id, params, socket) do
+    last_seen_id = params["last_seen_id"] || 0
     video_id = String.to_integer(video_id)
     video = Repo.get!(Rumbl.Videos.Video, video_id)
 
     annotations = Repo.all(
       from a in Ecto.assoc(video, :annotations),
+        where: a.id > ^last_seen_id,
         order_by: [asc: a.at, asc: a.id],
         limit: 200,
         preload: [:user]
